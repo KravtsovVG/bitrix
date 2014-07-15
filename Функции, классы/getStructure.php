697f9178b5e72e3a29e854a5ec87e53cd33520c7
NovikovRoman <service.for.roman@gmail.com>
2014-07-15 11:20:28 +0400

    getStructure

--- Функции, классы/getStructure.php
+++ Функции, классы/getStructure.php
@@ -46,18 +46,47 @@ function get_arr_category($_id, $this_section=false)
  return $res;
 }
 
-// noRoman
-function getStructure($id=0, $section=false, $stopID=0) {
+// noRoman (получить родительские разделы с пользовательскими свойствами )
+function getStructure($id, $section=false, $arUf=array()) {
   $id = (int)$id;
   if (!$id) { return false; }
-  if ($section) { $res = CIBlockSection::GetByID($id); } else { $res = CIBlockElement::GetByID($id); }
+  if ($section) {
+    $res = CIBlockSection::GetByID($id);
+  } else {
+    $res = CIBlockElement::GetByID($id);
+    if ( $ar = $res->GetNext() ){
+      $res = CIBlockSection::GetByID($ar['IBLOCK_SECTION_ID']);
+    } else { return false; }
+  }
   // херню подсунули
   if ( !$res->SelectedRowsCount() ) { return false; }
-  $ar = $res->GetNext();
+  $arSect = $res->GetNext();
+  $res = CIBlockSection::GetList(array('LEFT_MARGIN'=>'ASC'), array(
+    'IBLOCK_ID' => $arSect['IBLOCK_ID'],
+    '<=LEFT_BORDER' => $arSect['LEFT_MARGIN'],
+    '>=RIGHT_BORDER' => $arSect['RIGHT_MARGIN'],
+    '<DEPTH_LEVEL' => $arSect['DEPTH_LEVEL'],
+    '!UF_NOSHOW'=>false
+    ), $arUf);
   $arStructure = array();
-  while ( $ar['IBLOCK_SECTION_ID'] && $ar['IBLOCK_SECTION_ID']!=$stopID ) {
-    $res = CIBlockSection::GetByID($ar['IBLOCK_SECTION_ID']);
-    $ar = $res->GetNext();
+  while($ar = $res->GetNext()){
+    $arStructure[] = $ar;
+  }
+  return $arStructure;
+}
+// noRoman (получить родительские разделы без пользовательскими свойствами )
+function getStructure($id, $section=false) {
+  $id = (int)$id;
+  if (!$id) { return false; }
+  if (!$section) {
+    $res = CIBlockElement::GetByID($id);
+    if ( $ar = $res->GetNext() ){
+      $id = $ar['IBLOCK_SECTION_ID'];
+    } else { return false; }
+  }
+  $res = CIBlockSection::GetNavChain(false, $id);
+  $arStructure = array();
+  while($ar = $res->GetNext()){
     $arStructure[] = $ar;
   }
   return $arStructure;
